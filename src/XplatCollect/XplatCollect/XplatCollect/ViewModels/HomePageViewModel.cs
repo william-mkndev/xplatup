3640a111f7cbe77afbba1e82aab59d93c22cb07e
William <Rodriguez>
2019-09-14 10:41:46 -0300

    🚧 setup the viewmodel base

--- src/XplatCollect/XplatCollect/XplatCollect/ViewModels/HomePageViewModel.cs
+++ src/XplatCollect/XplatCollect/XplatCollect/ViewModels/HomePageViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
 
 namespace XplatCollect.ViewModels
 {
-    public sealed class HomePageViewModel : BindableBase
+    public sealed class HomePageViewModel : ViewModelBase
     {
         private string title;
+
+        public HomePageViewModel(INavigationService navigationService
+            , IPageDialogService pageDialogService) 
+            : base(navigationService, pageDialogService)
+        {
+            
+        }
+
         public string Title {
             get => title;
             set => SetProperty(ref title, value);
---
+++ src/XplatCollect/XplatCollect/XplatCollect/ViewModels/ViewModelBase.cs
@@ -0,0 +1,60 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XplatCollect.ViewModels
+{
+    public abstract class ViewModelBase : BindableBase, IInitializeAsync
+    {
+        private readonly INavigationService navigationService;
+        private readonly IPageDialogService pageDialogService;
+
+        protected ViewModelBase(INavigationService navigationService
+            , IPageDialogService pageDialogService)
+        {
+            this.navigationService = navigationService;
+            this.pageDialogService = pageDialogService;
+        }
+
+        private bool isBusy;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set => SetProperty(ref isBusy, value);
+        }
+
+        public bool IsNotBusy => !IsBusy;
+
+        public virtual Task InitializeAsync(INavigationParameters parameters)
+            => Task.CompletedTask;
+
+        protected async Task ExecuteBusyAction(Func<Task> theBusyAction)
+        {
+            try
+            {
+                if (IsBusy)
+                    return;
+
+                IsBusy = true;
+
+                await theBusyAction();
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                System.Diagnostics.Debug.WriteLine(ex);
+#endif
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+
+    }
+}
