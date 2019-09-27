67bc45d2d153232efc4ae2372fb178ac48fd04e6
William <Rodriguez>
2019-09-27 15:40:35 -0300

    profile pages

---
+++ src/XplatCollect/XplatCollect/ViewModels/ProfilePageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Navigation;
+using Prism.Services;
+
+namespace XplatCollect.ViewModels
+{
+    public sealed class ProfilePageViewModel : ViewModelBase
+    {
+        public ProfilePageViewModel(INavigationService navigationService
+            , IPageDialogService pageDialogService) 
+            : base(navigationService, pageDialogService)
+        {
+        }
+
+        private string userName;
+        public string UserName
+        {
+            get => userName;
+            set => SetProperty(ref userName, value);
+        }
+
+        private string userBio;
+        public string UserBio
+        {
+            get => userBio;
+            set => SetProperty(ref userBio, value);
+        }
+
+        private int collectionsQnt;
+        public int CollectionsQnt
+        {
+            get => collectionsQnt;
+            set => SetProperty(ref collectionsQnt, value);
+        }
+
+        private int itensQnt;
+        public int ItensQnt
+        {
+            get => itensQnt;
+            set => SetProperty(ref itensQnt, value);
+        }
+    }
+}
