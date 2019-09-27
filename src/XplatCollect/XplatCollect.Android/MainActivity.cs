36e0dd63e0b5150835b440d3f1bb20f8add7b60c
William S Rodriguez <williamsrz@outlook.com>
2019-09-27 20:04:07 -0300

    update android

--- src/XplatCollect/XplatCollect.Android/MainActivity.cs
+++ src/XplatCollect/XplatCollect.Android/MainActivity.cs
@@ -26,7 +26,7 @@ namespace XplatCollect.Droid
             Forms.SetFlags("CollectionView_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            LoadApplication(new App(new AndroidInitializer()));
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
